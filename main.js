34e24f40b51c5ab393635b2063dd5bbc132c3f15
randyttphan <randyttphan@gmail.com>
2020-01-30 15:28:26 -0600

    function to acquire proper field names and making first letter capitalized

--- main.js
+++ main.js
@@ -28,11 +28,15 @@ function emailValidation(email) {
 function checkRequired(inputArray) {
   inputArray.forEach(function(input) {
     if (input.value.trim() === "") {
-      showError(input, "Field Required");
+      showError(input, `${getFieldName(input)} is required`);
     } else {
       showSuccess(input);
     }
   });
+}
+
+function getFieldName(input) {
+  return input.id.charAt(0).toUpperCase() + input.id.slice(1);
 }
 
 form.addEventListener("submit", function(e) {
